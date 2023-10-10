5c940fb3693b1da4765b66c73402ddc7295fa389
mgrsc <migarishouchi@gmail.com>
2023-10-10 20:14:59 +0800

    Site updated: 2023-10-10 20:14:54

---
+++ js/title.js
@@ -0,0 +1,19 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '💔爱会消失对吗？';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '❤永远爱你！';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
+
+
